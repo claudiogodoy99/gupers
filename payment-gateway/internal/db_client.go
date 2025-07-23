fe54845f7295a65a300cdb363b43f718cca55ff0
claudiogodoy99 <cavalcantigodoy@gmail.com>
2025-07-23 15:29:58 -0300

    chore: type fixs

--- payment-gateway/internal/db_client.go
+++ payment-gateway/internal/db_client.go
@@ -8,10 +8,32 @@ import (
 
 type DBClient interface {
 	write(PaymentRequest) error
-	read(init, end time.Time) []Summary
+	read(init, end time.Time) [2]Summary
 }
 
 type Summary struct {
 	TotalRequests int
 	TotalAmount   decimal.Decimal
 }
+
+type ProcessorType int
+
+const (
+	Default ProcessorType = iota
+	Fallback
+)
+
+//expects
+// read: HTTP 200 - Ok
+// {
+//     "default" : {
+//         "totalRequests": 43236,
+//         "totalAmount": 415542345.98
+//     },
+//     "fallback" : {
+//         "totalRequests": 423545,
+//         "totalAmount": 329347.34
+//     }
+// }
+
+// write: err ou nil
